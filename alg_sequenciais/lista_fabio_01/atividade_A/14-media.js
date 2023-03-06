6f62eebd68c7c3b0764f8f7255e049f80a513ffe
Kleberson <klebisvillela@outlook.com>
2023-03-06 14:38:04 -0300

    corrigindo a questão

--- alg_sequenciais/lista_fabio_01/atividade_A/14-media.js
+++ alg_sequenciais/lista_fabio_01/atividade_A/14-media.js
@@ -7,10 +7,15 @@ const nota1 = Number(input('Nota 1: '))
 const nota2 = Number(input('Nota 2: '))
 const nota3 = Number(input('Nota 3: '))
 
-const peso1 = Number(input('\nPeso 1: '))
+console.log()
+
+const peso1 = Number(input('Peso 1: '))
 const peso2 = Number(input('Peso 2: '))
 const peso3 = Number(input('Peso 3: '))
 
-const media_ponderada = (nota1 * peso1) + (nota2 * peso2) + (nota3 * 3) / 3
+const soma_dos_pesos = peso1 + peso2 + peso3
+const somatorio = (nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)
 
-console.log(`\nValor da média ponderada: ${media_ponderada}`)
+const media_ponderada = somatorio / soma_dos_pesos 
+
+console.log(`\nSomatório: ${somatorio}\nSoma dos pesos ${soma_dos_pesos}\nValor da média ponderada: ${media_ponderada.toFixed(2)}`)
