0c0c8b1be5964b8fd9e5ba7dbdc2fe7ac2730fa7
Kleberson <klebisvillela@outlook.com>
2023-04-03 22:28:22 -0300

    retificada

--- prova1_g1/fies.js
+++ prova1_g1/fies.js
@@ -26,12 +26,13 @@ function main(){
     const valor_pago_na_carencia = calcular_valor_pago_apos_o_curso()
     
     const valorPago_antes_e_pos_curso = calcular_valor_antes_pos_curso(valor_pago_durante_o_curso, valor_pago_na_carencia)
-    const valor_total = calcular_valor_total(parecer_emprestimo, valor_fincancimento, total_juros, valorPago_antes_e_pos_curso)
+    const valor_total_financiamento = calcular_valor_total(valor_final, total_juros)
+    const valor_final = calcular_valor_final(valor_total_financiamento, valorPago_antes_e_pos_curso)
     
-    const valor_parcela_financiamento = calcular_valor_parcela_financiamento(parecer_emprestimo ,valor_total, duracaoCurso, tempoEmMeses)
+    const valor_parcela_financiamento = calcular_valor_parcela_financiamento(valor_final, duracaoCurso, tempoEmMeses)
     const renda_minima = calcular_renda_minima(rendaFamiliar)
     
-    const inicio_e_fim_do_pagamento = calcular_inicio_e_fim_do_pagamento(parecer_emprestimo, anoIngresso, tempoEmMeses)
+    const inicio_e_fim_do_pagamento = calcular_inicio_e_fim_do_pagamento(anoIngresso, tempoEmMeses)
     
     /*const informacao_fies =  imprimir_situacao(
         parecer_emprestimo, valor_fincancimento, total_juros, valor_total,
@@ -41,10 +42,11 @@ function main(){
         let informacao_fies
         informacao_fies = `\nSITUAÇÃO: ${parecer_emprestimo}`
         informacao_fies += `\nVALOR A SER FINANCIADO: R$ ${valor_fincancimento}`
-        informacao_fies += `\nVALOR TOTAL DE JUROS: R$ ${total_juros}`
+        informacao_fies += `\nVALOR TOTAL DE JUROS: R$ ${total_juros.toFixed(2)}`
+        informacao_fies += `\nVALOR TOTAL: R$ ${valor_total_financiamento}`
         informacao_fies += `\nVALOR PAGO DURANTE O CURSO: R$ ${valor_pago_durante_o_curso}`
         informacao_fies += `\nVALOR PAGO DURANTE A CARÊNCIA: R$ ${valor_pago_na_carencia}`
-        informacao_fies += `\nVALOR PAGO PÓS CARÊNCIA(TOTAL): R$ ${valor_total}`
+        informacao_fies += `\nVALOR PARCELA DO FINANCIAMENTO: R$ ${valor_parcela_financiamento}`
         informacao_fies += `\nRENDA MÍNIMA: R$${renda_minima}`
         informacao_fies += `\n${inicio_e_fim_do_pagamento}`
             
@@ -56,35 +58,30 @@ function main(){
 }
 
 function aprovar_contrato(renda, salario){
-    const qtdMaxSalario = 3
-    const qtdSalarioRendaCandidato = Math.trunc(renda / salario)
-    return qtdSalarioRendaCandidato >= qtdMaxSalario
+    const qtdMaxSalario = 3 * salario
+    const qtdRendaCandidato = Math.trunc(renda / salario)
+    return qtdRendaCandidato <= qtdMaxSalario
 }
+
 const mostrar_situacao_contrato = (situacao) => situacao === true ? 'CONTRATO APROVADO !':'CONTRATO NEGADO !'
 const converter_ano_para_meses = ano => ano * 12
 const calcular_renda_per_capita = (renda, qtd_pessoas) => (renda / qtd_pessoas) / 1000
 const calcular_valor_financiamento = (parcela, tempo_em_meses) => parcela * tempo_em_meses
     
 function calcular_total_juros(renda_per_capita, capital, selic, tempo){
-    //let taxaJuros
-    //let totalJuros
-    if (renda_per_capita >= 1.0 && renda_per_capita < 1.6){
-        //taxaJuros = selic * 0.1
-        return capital * (selic * 0.1) * tempo
+    
+    if (renda_per_capita >= 0 && renda_per_capita < 1.6){
+        return capital * ((selic * 0.1)/100) * tempo
     }
     else if(renda_per_capita < 2.1){
-        //taxaJuros = selic * 0.15
-        return capital * (selic * 0.1) * tempo
+        return capital * ((selic/100) * 0.15) * tempo
     }
     else if(renda_per_capita < 2.6){
-        //axaJuros = selic * 0.2
-        return capital * (selic * 0.2) * tempo
+        return capital * ((selic/100) * 0.2) * tempo
     }
     else{
-        //taxaJuros = selic * 0.25
-        return capital * (selic * 0.25) * tempo
+        return capital * ((selic/100) * 0.25) * tempo
     }
-    
 }
 
 const caluclar_valor_pago_durante_o_curso = tempo => 150 * (tempo / 3) 
@@ -93,40 +90,39 @@ const calcular_valor_antes_pos_curso = (valorNoCurso, valorPosCarencia) => valor
 const calcular_renda_minima = renda => renda / 3
 const eh_multiplo_de_6 = tempo_em_meses => tempo_em_meses % 6 === 0
 
-
-
-
-function calcular_inicio_e_fim_do_pagamento(parecer, ano, duracao_meses){
+function calcular_inicio_e_fim_do_pagamento(ano, duracao_meses){
     
-    let anoInicio
+    let anoInicioPagamento
     let semestreInicio
-    let anoFinal
+    let anoFinalPagamento
     let semestreFinal
     let inicio_e_fim_do_pagamento = ``
 
-    anoInicio = ano + Math.trunc((((duracao_meses + 18) / 12) - 1))
-    anoFinal = anoInicio + ((duracao_meses / 12) - 1)
-
-    if (parecer === 'CONTRATO APROVADO !'){
-        if (eh_multiplo_de_6(duracao_meses)){
-            if ((duracao_meses + 18) % 12 === 0){
-                semestreInicio = 1
-                semestreFinal = 2
-            }
-            if ((duracao_meses + 18) % 12 === 6){
-                semestreInicio = 2
-                semestreFinal = 1
-                anoFinal ++
-            }
+    anoInicioPagamento = ano + Math.trunc((((duracao_meses + 18) / 12) - 1))
+    anoFinalPagamento = anoInicioPagamento + Math.trunc(((duracao_meses / 12) * 4) - 1)
+   
+    if (eh_multiplo_de_6(duracao_meses)){
+        if ((duracao_meses + 18) % 12 === 0){
+            semestreInicio = 1
+            semestreFinal = 2
+        }
+        if ((duracao_meses + 18) % 12 === 6){
+            semestreInicio = 2
+            semestreFinal = 1
+            anoFinal ++
         }
     }
-    inicio_e_fim_do_pagamento += `INÍCIO DO PAGAMENTO: ${anoInicio}.${semestreInicio}`
-    inicio_e_fim_do_pagamento += `\nFIM DO PAGAMENTO: ${anoFinal}.${semestreFinal}`
+    inicio_e_fim_do_pagamento += `INÍCIO DO PAGAMENTO: ${anoInicioPagamento}.${semestreInicio}`
+    inicio_e_fim_do_pagamento += `\nFIM DO PAGAMENTO: ${anoFinalPagamento}.${semestreFinal}`
     
-
     return inicio_e_fim_do_pagamento
 }
 
+const calcular_valor_total = (financiamento, juros) => financiamento + juros
+const calcular_valor_final = (valorCurso, valorPosCarencia) => valorCurso - valorPosCarencia
+const calcular_valor_parcela_financiamento = (valorFinal, meses) => valorFinal / (meses * 4)   
+
+main()
 /*
 function imprimir_situacao(
     parecer, valorFincanciamento, juros,
@@ -147,19 +143,3 @@ function imprimir_situacao(
     return situacao
 }
 */
-
-function calcular_valor_total(parecer, financiamento, juros, valorPosCurso_e_Carencia)
-{   
-    let valor_total = (financiamento + juros) - valorPosCurso_e_Carencia
-    if (parecer === 'CONTRATO APROVADO !'){
-        return  valor_total
-    }
-}
-
-const calcular_valor_parcela_financiamento = (parecer, valorTotal, anos, meses) => {
-    if (parecer === 'CONTRATO APROVADO !'){
-        return ((valorTotal / anos) / meses).toFixed(2)
-    } 
-}
-
-main()
